4edd74d06154a054c65f502dc56e1c28f3987588
phamhongdinh <90923006+phamhongdinh@users.noreply.github.com>
2022-08-12 13:49:39 +0700

    init

---
+++ src/main/java/com/thanhtrong/lead/LeadApplication.java
@@ -0,0 +1,13 @@
+package com.thanhtrong.lead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadApplication.class, args);
+	}
+
+}
